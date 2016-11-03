adecf27f07187db31006f1c73936cc65d494c837
長井健介 <kaitoku2199@gmail.com>
2016-11-03 18:55:42 +0900

    First Commit

--- app.rb
+++ app.rb
@@ -1,3 +1,21 @@
 require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
+require 'net/http'
+require 'uri'
+require 'open-uri'
+require 'json'
+
+
+get '/' do
+    erb :index
+end
+
+get '/near' do
+   erb :near 
+end    
+
+
+get '/contact' do
+    erb :contact
+end    
